76fd7c75ca818def523fdbeeeae3dc5b8f94e3e6
AhmedBadrr310 <ahmedbadrr488@gmail.com>
2025-06-15 14:35:22 +0300

    removing listen

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
-import express from "express"
-import "dotenv/config"
-import tasksRouter from "./routes/tasks"
-import { json } from "express"
-import cors from 'cors'
+import express from "express";
+import "dotenv/config";
+import tasksRouter from "./routes/tasks";
+import { json } from "express";
+import cors from 'cors';
 
-const app = express()
-const port = process.env.PORT
+const app = express();
 
+// Middleware setup
 app.use(json());
-app.use(cors({ origin: '*', methods: '*', allowedHeaders: '*' }))
-app.use('/api', tasksRouter)
+app.use(cors({ origin: '*', methods: '*', allowedHeaders: '*' }));
 
+// Route setup
+app.use('/api', tasksRouter);
 
-//http://localhost:3000/
-app.listen(port);
+// Export the configured app
+export default app;
